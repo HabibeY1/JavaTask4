6bc8c74504d0a23e29b8b0be9371477a173a2006
HabibeY1 <habibeyavuz13@gmail.com>
2024-02-26 13:16:27 +0100

    First Commit

--- src/FirstJava.java
+++ src/FirstJava.java
@@ -1,7 +1,11 @@
 public class FirstJava {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Debugging starts...");
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+        System.out.println("Sum: " + sum);
         System.out.println("Hello Java");
-
     }
 }
